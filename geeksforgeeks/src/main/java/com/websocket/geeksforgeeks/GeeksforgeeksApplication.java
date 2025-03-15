4b2fcb0ed58e5e7bfca12f1e55fc7c29ec8162dd
edwinreyes7 <edwinreyesmartinez2@gmail.com>
2025-03-14 22:24:39 -0600

    Add files via upload

---
+++ geeksforgeeks/src/main/java/com/websocket/geeksforgeeks/GeeksforgeeksApplication.java
@@ -0,0 +1,13 @@
+package com.websocket.geeksforgeeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksforgeeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksforgeeksApplication.class, args);
+	}
+
+}
---
+++ geeksforgeeks/src/test/java/com/websocket/geeksforgeeks/GeeksforgeeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocket.geeksforgeeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksforgeeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
